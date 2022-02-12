95c4e98547f1db17f67d14ef776d4c87c82cdf46
Chris Martin <chris.martin@oforce.com>
2022-02-12 16:58:38 -0700

    fix supervisor name

--- lib/cqrs/application.ex
+++ lib/cqrs/application.ex
@@ -3,6 +3,6 @@ defmodule Cqrs.Application do
 
   def start(_type, _args) do
     [Cqrs.DispatchContext.Shipper]
-    |> Supervisor.start_link(strategy: :one_for_one, name: Insurance.Supervisor)
+    |> Supervisor.start_link(strategy: :one_for_one, name: Cqrs.Supervisor)
   end
 end

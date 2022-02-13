55956b4239de469691ff63c41f6380adf9d23989
Chris Martin <chris.martin@oforce.com>
2022-02-13 13:58:38 -0700

    pass message_type to pipeline resolver resolve function

--- lib/cqrs/dispatch_strategy/pipeline_resolver.ex
+++ lib/cqrs/dispatch_strategy/pipeline_resolver.ex
@@ -5,29 +5,30 @@ defmodule Cqrs.DispatchStrategy.PipelineResolver do
     defexception [:message]
   end
 
-  @type pipeline_module :: atom()
+  @type message_type :: atom()
   @type message_module :: atom()
+  @type pipeline_module :: atom()
   @type behaviour_module :: atom()
   @type context :: DispatchContext.command_context() | DispatchContext.query_context()
 
-  @callback resolve(message_module) :: {:ok, pipeline_module} | :error
+  @callback resolve(message_type, message_module) :: {:ok, pipeline_module} | :error
 
   @spec get_pipeline!(context, behaviour_module) :: pipeline_module
   @spec get_pipeline(context, behaviour_module) :: {:ok, pipeline_module} | {:error, String.t()} | :error
 
   @doc false
-  def get_pipeline(%{message: %{__struct__: module}}, behaviour_module) do
-    with {:ok, pipeline_module} <- Config.pipeline_resolver!().resolve(module) do
+  def get_pipeline(%{message_type: type, message_module: message_module}, behaviour_module) do
+    with {:ok, pipeline_module} <- Config.pipeline_resolver!().resolve(type, message_module) do
       Behaviour.validate(pipeline_module, behaviour_module)
     end
   end
 
   @doc false
-  def get_pipeline!(%{message: %{__struct__: module}} = context, behaviour_module) do
+  def get_pipeline!(%{message_module: message_module} = context, behaviour_module) do
     case get_pipeline(context, behaviour_module) do
       {:ok, pipeline} -> pipeline
       {:error, reason} -> raise Error, message: reason
-      :error -> raise Error, message: "No #{inspect(behaviour_module)} found for query: #{inspect(module)}"
+      :error -> raise Error, message: "No #{inspect(behaviour_module)} found for query: #{inspect(message_module)}"
     end
   end
 end
--- lib/cqrs/dispatch_strategy/pipeline_resolver/default.ex
+++ lib/cqrs/dispatch_strategy/pipeline_resolver/default.ex
@@ -8,12 +8,13 @@ defmodule Cqrs.DispatchStrategy.PipelineResolver.Default do
   That is, the message module with "Pipeline" appended to the end.
   """
 
-  @type pipeline_module :: atom()
+  @type message_type :: atom()
   @type message_module :: atom()
+  @type pipeline_module :: atom()
 
-  @spec resolve(message_module()) :: {:ok, pipeline_module()} | :error
+  @spec resolve(message_type(), message_module()) :: {:ok, pipeline_module()} | :error
 
-  def resolve(message_module) do
+  def resolve(_message_type, message_module) do
     {:ok, String.to_existing_atom(to_string(message_module) <> "Pipeline")}
   rescue
     _ -> :error

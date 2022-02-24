b5e2491ce5b6d8bceacd07858bf6e77467dcb03c
Chris Martin <chris.martin@oforce.com>
2022-02-23 20:52:54 -0700

    Configure package.

--- mix.exs
+++ mix.exs
@@ -1,10 +1,12 @@
 defmodule Blunt.MixProject do
   use Mix.Project
 
+  @version "0.1.0"
+
   def project do
     [
+      version: @version,
       app: :blunt,
-      version: "0.1.0",
       elixir: "~> 1.12",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
@@ -39,11 +41,16 @@ defmodule Blunt.MixProject do
       {:jason, "~> 1.3"},
       {:ecto, "~> 3.7"},
       {:decimal, "~> 1.6 or ~> 2.0"},
+
+      # Test and optional deps.
       {:etso, "~> 0.1.6", only: [:test]},
       {:faker, "~> 0.17.0", optional: true},
       {:ex_machina, "~> 2.7", optional: true},
       {:dialyxir, "~> 1.0", only: [:dev, :test], runtime: false},
-      {:elixir_uuid, "~> 1.6", override: true, hex: :uuid_utils}
+      {:elixir_uuid, "~> 1.6", override: true, hex: :uuid_utils},
+
+      # generate docs
+      {:ex_doc, "~> 0.28", only: :dev, runtime: false}
     ]
   end
 end

dcd916238c851264f3b291d2feb75914e819f4d1
Chris Martin <chris.martin@oforce.com>
2022-02-28 08:46:24 -0700

    support field provider field module aliases

--- apps/blunt/lib/blunt/message/schema/default_field_provider.ex
+++ apps/blunt/lib/blunt/message/schema/default_field_provider.ex
@@ -1,6 +1,7 @@
 defmodule Blunt.Message.Schema.DefaultFieldProvider do
   @behaviour Blunt.Message.Schema.FieldProvider
-  alias Blunt.Message.Schema.Fields
+  alias Blunt.Behaviour
+  alias Blunt.Message.Schema.{FieldProvider, Fields}
 
   @impl true
   def validate_changeset(validation, field_name, changeset, module) do
@@ -11,8 +12,8 @@ defmodule Blunt.Message.Schema.DefaultFieldProvider do
   end
 
   @impl true
-  def ecto_field(_module, {name, type, opts}) do
-    quote bind_quoted: [name: name, type: type, opts: opts] do
+  def ecto_field(module, {name, type, opts}) do
+    quote bind_quoted: [module: module, name: name, type: type, opts: opts] do
       case {name, type, opts} do
         {name, :binary_id, opts} ->
           Ecto.Schema.field(name, Ecto.UUID, opts)
@@ -29,9 +30,15 @@ defmodule Blunt.Message.Schema.DefaultFieldProvider do
             else: Ecto.Schema.field(name, {:array, type}, opts)
 
         {name, type, opts} ->
-          if Fields.embedded?(type),
-            do: Ecto.Schema.embeds_one(name, type),
-            else: Ecto.Schema.field(name, type, opts)
+          case Behaviour.validate(type, FieldProvider) do
+            {:ok, provider} ->
+              provider.ecto_field(module, {name, type, opts})
+
+            _otherwise ->
+              if Fields.embedded?(type),
+                do: Ecto.Schema.embeds_one(name, type),
+                else: Ecto.Schema.field(name, type, opts)
+          end
       end
     end
   end

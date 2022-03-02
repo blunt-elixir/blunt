9db97115bda7e2d2e45247ec6efc469a2481213d
Chris Martin <chris.martin@oforce.com>
2022-03-02 00:54:08 -0700

    remve temp files

--- apps/blunt_data/lib/blunt/data/factories/values.ex
+++ apps/blunt_data/lib/blunt/data/factories/values.ex
@@ -1,88 +1,86 @@
-if Code.ensure_loaded?(ExMachina) and Code.ensure_loaded?(Faker) do
-  defmodule Blunt.Data.Factories.Values do
-    defmodule Constant do
-      @moduledoc false
-      @derive Inspect
-      defstruct [:field, :value]
-    end
+defmodule Blunt.Data.Factories.Values do
+  defmodule Constant do
+    @moduledoc false
+    @derive Inspect
+    defstruct [:field, :value]
+  end
 
-    @doc """
-    The `field` of the factory source data will be assigned
-    to the value
-    """
-    defmacro const(field, value) do
-      quote do
-        %Constant{field: unquote(field), value: unquote(value)}
-      end
+  @doc """
+  The `field` of the factory source data will be assigned
+  to the value
+  """
+  defmacro const(field, value) do
+    quote do
+      %Constant{field: unquote(field), value: unquote(value)}
     end
+  end
 
-    defmodule Data do
-      @moduledoc false
-      @derive Inspect
-      defstruct [:field, :factory, lazy: false]
-    end
+  defmodule Data do
+    @moduledoc false
+    @derive Inspect
+    defstruct [:field, :factory, lazy: false]
+  end
 
-    @doc """
-    If the `field` is not supplied to the factory,
-    the given `message` will be dispatched with the returned
-    data to be put into the factory source data under the `field` key.
-    """
-    defmacro data(field, message, values \\ []) do
-      quote do
-        %Data{
-          field: unquote(field),
-          factory: %{
-            name: :dependency,
-            message: unquote(message),
-            values: unquote(values),
-            dispatch?: true
-          }
+  @doc """
+  If the `field` is not supplied to the factory,
+  the given `message` will be dispatched with the returned
+  data to be put into the factory source data under the `field` key.
+  """
+  defmacro data(field, message, values \\ []) do
+    quote do
+      %Data{
+        field: unquote(field),
+        factory: %{
+          name: :dependency,
+          message: unquote(message),
+          values: unquote(values),
+          dispatch?: true
         }
-      end
+      }
     end
+  end
 
-    @doc """
-    Same as `data` but
-    """
-    defmacro lazy_data(field, message, values \\ []) do
-      quote do
-        %Data{
-          lazy: true,
-          field: unquote(field),
-          factory: %{
-            name: :dependency,
-            message: unquote(message),
-            values: unquote(values),
-            dispatch?: true
-          }
+  @doc """
+  Same as `data` but
+  """
+  defmacro lazy_data(field, message, values \\ []) do
+    quote do
+      %Data{
+        lazy: true,
+        field: unquote(field),
+        factory: %{
+          name: :dependency,
+          message: unquote(message),
+          values: unquote(values),
+          dispatch?: true
         }
-      end
+      }
     end
+  end
 
-    defmodule Prop do
-      @moduledoc false
-      @derive {Inspect, except: [:lazy]}
-      defstruct [:field, :value_path_or_func, lazy: false]
+  defmodule Prop do
+    @moduledoc false
+    @derive {Inspect, except: [:lazy]}
+    defstruct [:field, :value_path_or_func, lazy: false]
+  end
+
+  @doc """
+  The `field` of the factory source data will be assigned
+  to the value of `value_path_or_func` in the factory source
+  """
+  defmacro prop(field, value_path_or_func) do
+    quote do
+      %Prop{field: unquote(field), value_path_or_func: unquote(value_path_or_func)}
     end
+  end
 
-    @doc """
-    The `field` of the factory source data will be assigned
-    to the value of `value_path_or_func` in the factory source
-    """
-    defmacro prop(field, value_path_or_func) do
-      quote do
-        %Prop{field: unquote(field), value_path_or_func: unquote(value_path_or_func)}
-      end
-    end
-
-    @doc """
-    The `field` of the factory source data will be assigned
-    to the value of `value_path_or_func` in the factory source
-    """
-    defmacro lazy_prop(field, value_path_or_func) do
-      quote do
-        %Prop{field: unquote(field), value_path_or_func: unquote(value_path_or_func), lazy: true}
-      end
+  @doc """
+  The `field` of the factory source data will be assigned
+  to the value of `value_path_or_func` in the factory source
+  """
+  defmacro lazy_prop(field, value_path_or_func) do
+    quote do
+      %Prop{field: unquote(field), value_path_or_func: unquote(value_path_or_func), lazy: true}
     end
   end
 end

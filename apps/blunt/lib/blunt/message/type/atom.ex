24b0126ec7037ed2c970431afcfed84af14501ec
Chris Martin <chris.martin@oforce.com>
2022-05-23 17:44:46 -0700

    fix atom type

--- apps/blunt/lib/blunt/message/type/atom.ex
+++ apps/blunt/lib/blunt/message/type/atom.ex
@@ -5,14 +5,19 @@ defmodule Blunt.Message.Type.Atom do
 
   def type, do: :any
 
+  def cast(nil), do: {:ok, nil}
+
   def cast(atom) when is_atom(atom),
     do: {:ok, atom}
 
-  # def cast(string) when is_binary(string),
-  #   do: {:ok, String.to_existing_atom(string)}
+  def cast(_other), do: :error
+
+  def load(nil), do: {:ok, nil}
 
   def load(value) when is_binary(value),
     do: {:ok, String.to_existing_atom(value)}
+
+  def dump(nil), do: {:ok, nil}
 
   def dump(value) when is_atom(value),
     do: {:ok, to_string(value)}
